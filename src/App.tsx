14b9e78d8844835725c0b0b0df198a2592a01670
TorenUK <td_fraser@hotmail.co.uk>
2020-11-25 14:27:21 +0000

    add card product

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
 // components
+import "./App.css";
 import Card from "./components/Card";
 
 // utils
-import { getTotal } from "./utils";
+
+// other
+import image from "./images/nike-trainers.jpg";
 
 interface State {
   totalPrice: number;
   quantity: number;
+  product: Array<{ price: number; image: string }>;
 }
 
 class App extends React.Component<{}, State> {
@@ -16,12 +20,19 @@ class App extends React.Component<{}, State> {
     this.state = {
       totalPrice: 0,
       quantity: 0,
+      product: [
+        {
+          price: 500,
+          image:
+            "https://static.highsnobiety.com/thumbor/VbyAvn9l8mFcDAMIs905HxgsJeo=/1600x1067/static.highsnobiety.com/wp-content/uploads/2019/09/09113208/nike-sacai-ldwaffle-main1.jpg",
+        },
+      ],
     };
-    const { totalPrice, quantity } = this.state;
+    const { totalPrice, quantity, product } = this.state;
 
     return (
       <div className="app">
-        <Card totalPrice={totalPrice} quantity={quantity} />
+        <Card totalPrice={totalPrice} quantity={quantity} product={product} />
       </div>
     );
   }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 // components
+import "./styles/Card.css";
 
 type Props = {
   totalPrice: number;
   quantity: number;
+  product: Array<{ price: number; image: string }>;
 };
 
 class Card extends React.Component<Props, {}> {
@@ -13,10 +15,20 @@ class Card extends React.Component<Props, {}> {
   }
 
   render() {
+    const product = this.props.product;
+
     return (
       <div className="card">
-        <p>total price:{this.props.totalPrice}</p>
-        <p>quantity:{this.props.quantity}</p>
+        {product.map((product) => (
+          <div className="card__product">
+            <h3>£{product.price}</h3>
+            <img src={product.image} />
+          </div>
+        ))}
+        <div className="card__interact">
+          <p>total price:{this.props.totalPrice}</p>
+          <p>quantity:{this.props.quantity}</p>
+        </div>
       </div>
     );
   }
